327506933e174d84aa5b92e7d670c714b67f0079
Manuel Escudero <arkaninger@gmail.com>
2019-12-06 18:58:24 +0100

    Forgotten things

--- Day01/part01.py
+++ Day01/part01.py
@@ -8,6 +8,5 @@ if __name__ == '__main__':
     with open(INPUT_FILEPATH, encoding='utf-8') as input_file:
         masses = input_file.read().splitlines()
         for mass in masses:
-            int_mass = int(mass)
             fuel += (int(mass) // 3) - 2
         print("Total fuel required {0}".format(fuel))
